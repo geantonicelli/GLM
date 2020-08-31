2a4129d7bc1e5b989405b0a78bbdaaf5a024a0c9
gerardo antonicelli <esteban.antonicelli@gmail.com>
2020-08-31 20:55:58 +0200

    new function fit_aov() omega() function improved

--- R/functions.R
+++ R/functions.R
@@ -390,32 +390,148 @@ corr.test <- function(data, method='pearson', alternative='two.sided',
 #' @importFrom boot boot boot.ci
 #'
 #' @export
+# my omega_factorial() function based on page 542 of Discovering Statistics Using R: and Sex and Drugs and Rock 'N' Roll
 omega <- function(anova){
   ee <<- environment() # trick to see what's inside of the function's execution environment
   print(environment())
   # print(ls.str()) # alternatively
   # ------------------------------------------------------------------------------------------
-  model <- summary(anova)
-  SSm <- model[[1]][[2]]
-  MSr <- model[[1]][[3]][[length(SSm)]]
-  SSt <- sum(SSm)
-  df <- model[[1]][[1]]
-  omega.square <- c()
-  f.square <- c()
-  for(i in 1:length(SSm)-1){
-    i.SSm <- SSm[i]
-    i.df <- df[i]
-    num <- i.SSm-(i.df*MSr)
-    den <- SSt+MSr
-    omega <- num/den
-    omega.square[i] <- omega
-    f <- omega/(1-omega)
-    f.square[i] <- f
+  contr <- fit_aov(anova)
+  ncontr <- length(contr)
+  colect <- vector('list', length=ncontr+1)
+  var.nam <- c('main_ANOVA', names(contr))
+  out <- vector('list', length=ncontr+1)
+  colect[[1]] <- anova
+  for(i in 1:ncontr){
+    colect[[i+1]] <- contr[[i]]
+    names(colect[[i+1]]) <- names(contr[[i]])
   }
-  names <- rownames(model[[1]])
-  names <- names[1:length(SSm)-1]
-  names(omega.square) <- names
-  out <- as.matrix(as.data.frame(omega.square))
-  out <- cbind(out, f.square)
-  return(out)
+  nloop <- length(colect)
+  for(j in 1:nloop){
+    loop <- colect[[j]]
+    if(length(loop)==0) next
+    model <- summary(loop)
+    SSm <- model[[1]][[2]]
+    MSr <- model[[1]][[3]][[length(SSm)]]
+    SSt <- sum(SSm)
+    df <- model[[1]][[1]]
+    omega.square <- c()
+    f.square <- c()
+    for(k in 1:(length(SSm)-1)){
+      i.SSm <- SSm[k]
+      i.df <- df[k]
+      num <- i.SSm-(i.df*MSr)
+      den <- SSt+MSr
+      omega <- num/den
+      omega.square[k] <- omega
+      f <- omega/(1-omega)
+      f.square[k] <- f
+    }
+    names <- rownames(model[[1]])
+    names <- names[1:length(SSm)-1]
+    names(omega.square) <- names
+    res <- as.matrix(as.data.frame(omega.square))
+    res <- cbind(res, f.square)
+    out[[j]] <- res
+  }
+  names(out) <- var.nam
+  out
+}
+
+#' function to calculate the effect size of a comparison
+#'
+#' this function is a wrapper and an adapter around the functions
+#'   \code{'\link[pastecs]{stat.desc}'} and \code{'\link[compute.es]{mes}'} it
+#'   takes a data.frame with variables and calculate the effect size of a
+#'   comparison between two chunks of variation, they could be two levels of one
+#'   factor, the combined effect of more than one level of a factor vs another
+#'   level or between two levels of a factor constrained to one level of another
+#'   factor, what is called simple effects analysis, all this possible comparisons
+#'   between two chunks can be analysed with the same function by using orthogonal
+#'   contrasts coded inside the data.frame as columns of dummy variables with the
+#'   weights representing the comparison worth to be analysed closer
+#'
+#' @param data a character string without '' specifying a data.frame object with
+#'   the data, each variable must be in only one column, one dummy variable with
+#'   weights (in one column) for each contrast to be analysed must be provided
+#' @param dep a character string without '' specifying the name of the dependent
+#'   variable, this must be at the same time a column name in the data object
+#' @param contrast a character string without '' specifying the name of the
+#'   contrast to be analysed, this must be at the same time the name of a column
+#'   for a dummy variable with weights specifying which samples should be
+#'   compared
+#' @param dig numeric an integer specifying the number of decimal digits to be
+#'   printed out and also invisibly returned by the mes() function
+#'
+#' @return the function returns invisibly a data.frame with all coefficients
+#'   from the calculation of effect size, in addition it prints out a summary
+#'   with the most important coefficients
+#'
+#' @author gerardo esteban antonicelli
+#'
+#' @seealso \code{'\link{check_contrasts}'} \code{'\link{omega_factorial}'}
+#'
+#' @aliases \alias{es}
+#'
+#' @examples
+#' data(gogglesDataES)
+#' data(depressionDataES)
+#' es(gogglesDataES, attractiveness, alcEffect1)
+#' es(gogglesDataES, attractiveness, alcEffect2)
+#' es(gogglesDataES, attractiveness, gender_none)
+#' es(gogglesDataES, attractiveness, gender_twoPints)
+#' es(gogglesDataES, attractiveness, gender_fourPints)
+#' es(depressionDataES, diff, all_vs_NoTreatment, dig=4)
+#' es(depressionDataES, diff, treatment_vs_placebo)
+#' es(depressionDataES, diff, old_vs_newDrug, dig=2)
+#' es(depressionDataES, diff, old_vs_oldDrug)
+#'
+#' @importFrom boot boot boot.ci
+#'
+#' @export
+fit_aov <- function(anova){
+  ee <<- environment() # trick to see what's inside of the function's execution environment
+  # print(environment())
+  # print(ls.str()) # alternatively
+  # ------------------------------------------------------------------------------------------
+  require(dplyr)
+  aovcall <- anova$call
+  formula <- formula(aovcall)
+  y <- deparse(formula[[2]])
+  model <- anova[length(anova)]
+  nvar <- dim(model$model)[2]
+  Ntotal <- dim(model$model)[1]
+  fits <- vector('list', nvar)
+  names(fits) <- colnames(model$model)
+  for(i in 1:nvar){
+    var <- attr(model$model[[i]], 'contrasts')
+    if(is.null(var)) next
+    ncol <- dim(var)[2]
+    contrasts <- vector('list', ncol)
+    names(contrasts) <- colnames(var)
+    n <- Ntotal/length(levels(model$model[[i]]))
+    for(j in 1:ncol){
+      contrast <- var[ , j]
+      weights <- rep(contrast, each=n)
+      contrasts[[j]] <- weights
+    }
+    contrasts <- as.data.frame(contrasts)
+    dataset <- arrange(model$model, model$model[[i]])
+    dep <- dataset[y]
+    data <- cbind(dep, contrasts)
+    indep <- colnames(contrasts)
+    ind.for <- indep[1]
+    if(length(indep)>=2){
+      for(k in 2:length(indep)){
+        ind.for <- paste0(ind.for, ' + ', indep[k])
+      }
+    }
+    aovcall[[1L]] <- quote(stats::aov)
+    aovcall$formula <- as.formula(paste0(y, ' ~ ', ind.for))
+    aovcall$data <- quote(data)
+    fit <- eval(substitute(aovcall))
+    fits[[i]] <- fit
+    out <- fits[-1]
+  }
+  out
 }
